bc91ca5ee4c0b5ee3ce20b20b14735c3292b98ff
franzley <franzleybonhomme003@gmail.com>
2024-05-17 16:01:19 -0400

    Added logic for webpage is scrolled when page refreshes, added testimonials section, added comments

--- src/utilsJS/navbar.js
+++ src/utilsJS/navbar.js
@@ -5,6 +5,14 @@
 ////////////////////////////////////////
 
 const navbar = () => {
+  // If the page is refreshed and the window is scrolled down
+  window.addEventListener("load", () => {
+    if (window.scrollY > 0) {
+      document.querySelector("nav").classList.add("window-scroll");
+    } else {
+      document.querySelector("nav").classList.remove("window-scroll");
+    }
+  });
   // Change navbar on scroll
   window.addEventListener("scroll", () => {
     // When the page is scrolling, apply class to nav
