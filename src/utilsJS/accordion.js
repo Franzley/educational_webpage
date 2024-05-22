f2863fb0270df28bb7610c957f2b8cf03f4d3555
franzley <franzleybonhomme003@gmail.com>
2024-05-22 18:10:57 -0400

    changed icon svgs for css span, changed container structure for faq triggers

--- src/utilsJS/accordion.js
+++ src/utilsJS/accordion.js
@@ -11,9 +11,6 @@ const accordion = () => {
     const panel = accordion.querySelector(".accordion-panel");
     const trigger = accordion.querySelector(".accordion-trigger");
     // Select path tag of the svg
-    const icon = accordion
-      .querySelector(".accordion-icon svg")
-      .querySelector("path");
 
     // State
     var isCollapsed = false;
@@ -24,19 +21,10 @@ const accordion = () => {
         // Display answer
         panel.style.height = `${panel.scrollHeight}px`;
         // Change svg to minus sign
-        icon.setAttribute("d", "M6 12L18 12");
-        icon.setAttribute("stroke", "currentColor");
-        icon.setAttribute("stroke-width", "2");
       } else {
         // Hide answer
         panel.style.height = "0";
         // Change svg to plus
-        icon.setAttribute(
-          "d",
-          "M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"
-        );
-        icon.removeAttribute("stroke");
-        icon.removeAttribute("stroke-width");
       }
     });
   });
